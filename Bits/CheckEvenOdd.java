411e3098ef0cf1581133160cecd4c19d7f42380d
Ranj1thaa <r.17ranjitha@gmail.com>
2026-03-12 16:26:01 +0530

    Solving bits

---
+++ Bits/CheckEvenOdd.java
@@ -0,0 +1,6 @@
+//LSB (odd always 1, even 0)
+public class CheckEvenOdd {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ KthLargestElement.java
@@ -0,0 +1,6 @@
+public class KthLargestElement {
+  public static void main(String[] args) {
+    int[] nums = { 3, 2, 1, 5, 6, 4 };
+    int k = 2;
+  }
+}
